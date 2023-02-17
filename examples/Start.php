791c5675b255a4582165bd6b9eb36a350a3d037f
Mateo <orehurcomito@gmail.com>
2023-02-16 22:16:06 -0500

    update examples

--- examples/Start.php
+++ examples/Start.php
@@ -1,6 +1,7 @@
 <?php
 
 use Mateodioev\Bots\Telegram\Api;
+use Mateodioev\Request\Request;
 use Mateodioev\TgHandler\Commands\MessageCommand;
 
 class Start extends MessageCommand
@@ -8,13 +9,16 @@ class Start extends MessageCommand
 	protected string $name = 'start';
     protected string $description = 'Start the command';
 
-	/**
-	 * Run command
-	 */
+    /**
+     * Run command
+     * @throws Exception
+     */
 	public function handle(Api $bot, Mateodioev\TgHandler\Context $context)
 	{
 		$bot->sendMessage($context->getChatId(), 'Hello world!');
-
 		$this->getLogger()->info('Received new text: ' . $context->message()->text());
+
+        // This will throw an exception
+        Request::GET('https://invalidurl.invalid')->run();
 	}
 }
--- examples/bot.php
+++ examples/bot.php
@@ -1,16 +1,22 @@
 <?php
 
 use Mateodioev\TgHandler\Log\{FileStream, Logger};
+use Mateodioev\TgHandler\{Bot, Context};
+use Mateodioev\Utils\Exceptions\RequestException;
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/Start.php';
 
-\Dotenv\Dotenv::createImmutable(__DIR__ . '/../')->load();
-
-$bot = new \Mateodioev\TgHandler\Bot($_ENV['BOT_TOKEN']);
+$bot = new Bot('2072676984:AAFP7pTgokA5mDTPTMRvlcW5VJSpp4g4biA');
 
 // Log in file
 $bot->setLogger(new Logger(FileStream::fromToday(__DIR__)));
+
+$bot->setExceptionHandler(RequestException::class, function (RequestException $e, Bot $bot, Context $ctx) {
+    echo 'RequestException: ' . $e->getMessage() . PHP_EOL;
+
+    $bot->getLogger()->warning($e->getMessage());
+});
 
 $bot->on('message', Start::get())
     ->longPolling(20);
