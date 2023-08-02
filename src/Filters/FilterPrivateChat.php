a5062635093f2aa4ac5678f3f8f45897c6524912
Mateo O <orehurcomito@gmail.com>
2023-08-02 17:21:29 -0500

    rename class

--- src/Filters/FilterPrivateChat.php
+++ src/Filters/FilterPrivateChat.php
@@ -8,7 +8,7 @@ use Mateodioev\TgHandler\Context;
  * This filter validates that the chat is private
  */
 #[\Attribute]
-final class FilterMessageRegex implements Filter
+final class FilterPrivateChat implements Filter
 {
 	public function apply(Context $ctx): bool
 	{
