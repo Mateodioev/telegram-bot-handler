01eb26d5e0b58229c5f149fe1e5a958abfaca6ed
Mateo O <orehurcomito@gmail.com>
2023-08-02 22:24:52 -0500

    update psr-4 name

--- src/Filters/FilterXor.php
+++ src/Filters/FilterXor.php
@@ -8,7 +8,7 @@ use Mateodioev\TgHandler\Context;
  * Return true is `$a` or `$b` return true after call method {@see Filter::apply}, but if both return true, `apply` return false
  */
 #[\Attribute]
-final class FilterOr implements Filter
+final class FilterXor implements Filter
 {
 	function __construct(
 		private Filter $a,
