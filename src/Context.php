e98a87c9d92119e3b3934588d969b6281233a982
Mateo O <orehurcomito@gmail.com>
2023-08-09 22:50:12 -0500

    The methods of the Context class were accelerated
    x10 more faster

--- src/Context.php
+++ src/Context.php
@@ -2,6 +2,7 @@
 
 namespace Mateodioev\TgHandler;
 
+use Closure;
 use Mateodioev\Bots\Telegram\Types\{
     Document,
     Update,
@@ -18,6 +19,7 @@ use function explode, substr, strlen, trim;
 class Context extends Update
 {
     private ?EventType $type = null;
+    private array $cache = [];
 
     public static function fromUpdate(Update $update): Context
     {
@@ -26,10 +28,12 @@ class Context extends Update
 
     public function getUser(): ?User
     {
-        return $this->message()?->from()
-            ?? $this->callbackQuery()?->from()
-            ?? $this->inlineQuery()?->from()
-            ?? null;
+        return $this->resolve('user', function () {
+            return $this->message()?->from()
+                ?? $this->callbackQuery()?->from()
+                ?? $this->inlineQuery()?->from()
+                ?? null;
+        });
     }
 
     public function getUserId(): ?int
@@ -44,64 +48,82 @@ class Context extends Update
 
     public function getChatId(): ?int
     {
-        return $this->message()?->chat()?->id()
-            ?? $this->callbackQuery()?->message()?->chat()?->id()
-            ?? null;
+        return $this->resolve('chat_id', function () {
+            return $this->message()?->chat()?->id()
+                ?? $this->callbackQuery()?->message()?->chat()?->id()
+                ?? null;
+        });
     }
 
     public function getMessageId(): ?int
     {
-        return $this->message()?->messageId()
-            ?? $this->callbackQuery()?->message()?->messageId()
-            ?? null;
+        return $this->resolve('message_id', function () {
+            return $this->message()?->messageId()
+                ?? $this->callbackQuery()?->message()?->messageId()
+                ?? null;
+        });
     }
 
     public function getMessageText(): ?string
     {
-        return $this->message()?->text()
-            ?? $this->callbackQuery()?->data()
-            ?? $this->inlineQuery()?->query()
-            ?? null;
+        return $this->resolve('message_text', function () {
+            return $this->message()?->text()
+                ?? $this->callbackQuery()?->data()
+                ?? $this->inlineQuery()?->query()
+                ?? null;
+        });
     }
 
     public function getPayload(): string
     {
-        $text = $this->getMessageText() ?? '';
-        $command = explode(' ', $text)[0] ?? '';
-        return substr($text, strlen($command) + 1);
+        return $this->resolve('message_payload', function () {
+            $text = $this->getMessageText() ?? '';
+            $command = explode(' ', $text)[0] ?? '';
+            return substr($text, strlen($command) + 1);
+        });
     }
 
     public function getChatType(): ?string
     {
-        return $this->message()?->chat()?->type()
-            ?? $this->callbackQuery()?->message()?->chat()?->type()
-            ?? null;
+        return $this->resolve('chat_type', function () {
+            return $this->message()?->chat()?->type()
+                ?? $this->callbackQuery()?->message()?->chat()?->type()
+                ?? null;
+        });
     }
 
     public function getFullName(): string
     {
-        return trim($this->getFirsName() . ' ' . $this->getLastName());
+        return $this->resolve('user_full_name', function () {
+            return trim($this->getFirsName() . ' ' . $this->getLastName());
+        });
     }
 
     public function getFirsName(): ?string
     {
-        return $this->getUser()?->firstName()
-            ?? $this->message()?->replyToMessage()?->from()?->firstName()
-            ?? null;
+        return $this->resolve('first_name', function () {
+            return $this->getUser()?->firstName()
+                ?? $this->message()?->replyToMessage()?->from()?->firstName()
+                ?? null;
+        });
     }
 
     public function getLastName(): ?string
     {
-        return $this->getUser()?->lastName()
-            ?? $this->message()?->replyToMessage()?->from()?->lastName()
-            ?? null;
+        return $this->resolve('last_name', function () {
+            return $this->getUser()?->lastName()
+                ?? $this->message()?->replyToMessage()?->from()?->lastName()
+                ?? null;
+        });
     }
 
     public function getDocument(): ?Document
     {
-        return $this->message()?->document()
-            ?? $this->message()?->replyToMessage()?->document()
-            ?? null;
+        return $this->resolve('document', function () {
+            return $this->message()?->document()
+                ?? $this->message()?->replyToMessage()?->document()
+                ?? null;
+        });
     }
 
     /**
@@ -116,7 +138,7 @@ class Context extends Update
         unset($eventTypes['update_id']);
 
         foreach ($eventTypes as $type => $value) {
-            if ($value !== null && $value instanceof TypesInterface) {
+            if ($value instanceof TypesInterface) {
                 $this->type = EventType::silentFrom($type);
                 return $this->type;
             }
@@ -124,5 +146,29 @@ class Context extends Update
 
         $this->type = EventType::none;
         return $this->type;
+    }
+
+    /**
+     * Resolve a value
+     * 
+     * @template T
+     * 
+     * @param string $key
+     * @param \Closure(...): T $callback
+     * 
+     * @return T
+     */
+    private function resolve(string $key, Closure $callback)
+    {
+        if (isset($this->cache[$key]))
+            return $this->cache[$key];
+
+        $this->cache[$key] = $callback();
+        return $this->cache[$key];
+    }
+
+    public function clearCache(): void
+    {
+        $this->cache = [];
     }
 }
